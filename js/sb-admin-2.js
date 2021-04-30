17fc34dd6e0d80720304a32b7dfa17e2702b6846
yudhakrisnawan <yudhakrisnawan1922@gmail.com>
2021-04-30 23:20:15 +0700

    Rapiin kode

--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -34,17 +34,7 @@
     }
   });
 
-  // Scroll to top button appear
-  $(document).on('scroll', function() {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
-    } else {
-      $('.scroll-to-top').fadeOut();
-    }
-  });
-
-  // Smooth scrolling using jQuery easing
+  // Smooth scrolling 
   $(document).on('click', 'a.scroll-to-top', function(e) {
     var $anchor = $(this);
     $('html, body').stop().animate({
